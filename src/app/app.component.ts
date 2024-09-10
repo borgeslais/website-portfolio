44f1986f684417da74f76b6c74271faa7b2c6ac7
borgeslais <borgescrlais@gmail.com>
2024-09-10 20:26:22 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,88 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { RouterLink, RouterOutlet, RouterLinkActive } from '@angular/router';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterLink, RouterOutlet, RouterLinkActive, NgStyle],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'ang-learning';
+  imageSrc = "assets/dark-sun.svg";
+  moonSrc = "assets/dark-moon.svg"
+
+  linkedinSrc = "assets/dark-linkedin";
+  githubSrc = "assets/dark-github";
+
+  isLinksVisible: boolean = false;
+
+  toggleLinks(): void {
+    this.isLinksVisible = !this.isLinksVisible;
+  }
+  
+  constructor() {
+    
+  };
+  
+  ngOnInit(): void { 
+    this.applySystemThemePreference();
+    this.updateThemeIcon();
+
+    // Listen for changes in the system's color scheme preference
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+      const isDarkMode = event.matches;
+      this.setTheme(isDarkMode);
+    });
+
+  };
+
+  public toggle() {
+    document.body.classList.toggle('dark-theme');
+    this.updateThemeIcon();
+  }
+
+  private applySystemThemePreference() {
+    const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    this.setTheme(isDarkMode);
+  }
+
+  private setTheme(isDarkMode: boolean) {
+    if (isDarkMode) {
+      document.body.classList.add('dark-theme');
+    } else {
+      document.body.classList.remove('dark-theme');
+    }
+    this.updateThemeIcon();
+  }
+
+  private updateThemeIcon() {
+    if (document.body.classList.contains('dark-theme')) {
+      this.imageSrc = "assets/light-sun.svg";  // Image for dark mode
+    } else {
+      this.imageSrc = "assets/dark-sun.svg";  // Image for light mode
+    }
+
+    if (document.body.classList.contains('dark-theme')) {
+      this.moonSrc = "assets/light-moon.svg";  // Image for dark mode
+    } else {
+      this.moonSrc = "assets/dark-moon.svg";  // Image for light mode
+    }
+
+    if (document.body.classList.contains('dark-theme')) {
+      this.linkedinSrc = "assets/light-linkedin.svg";  // Image for dark mode
+    } else {
+      this.linkedinSrc = "assets/dark-linkedin.svg";  // Image for light mode
+    }
+
+    if (document.body.classList.contains('dark-theme')) {
+      this.githubSrc = "assets/light-github.svg";  // Image for dark mode
+    } else {
+      this.githubSrc = "assets/dark-github.svg";  // Image for light mode
+    }
+    
+
+  }
 }
+
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
+import { AppComponent } from './app.component';
 import { Routes } from '@angular/router';
+import { HtmlComponent } from './html/html.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: '',
+        title: 'Sobre mim',
+        component: AboutComponent,
+    },
+    {
+        path: 'html',
+        title: 'HTML, CSS, JavaScript',
+        component: HtmlComponent,
+    },
+];
